c0e0d76a015c631a57fb88b8b52da7b5b9bf9018
przemyslawpluta <przemekpluta@hotmail.com>
2019-05-24 10:49:43 +0100

    upscale non png typo fix and test

--- tests/filePreserveAPI.js
+++ tests/filePreserveAPI.js
@@ -211,6 +211,104 @@ describe('# file preserve workflow service API', () => {
 
     });
 
+    context('with correct source, same destination a 5k image with person and size auto detected and alpha channels set but resolution preserved', () => {
+
+        let outcome = {};
+        let sourceFile = {};
+        let destinationFile = {};
+        const testFile = `${dir}/ultrahd/${testFile_5K}.jpg`;
+
+        it('should return object', async () => {
+
+            outcome = await removd.file({
+                preserve: true,
+                channels: 'alpha',
+                source: testFile
+            });
+
+            expect(outcome).to.be.an('object').that.has.all.keys('charged', 'size', 'duration', 'dimensions', 'destination', 'resized', 'detected', 'preserved');
+
+            sourceFile = path.parse(testFile);
+            destinationFile = path.parse(outcome.destination);
+
+        });
+
+        it('should preserve original source file', async () => {
+            const originalSource = await common.currentFile(testFile);
+            expect(originalSource).to.deep.equal({
+                exists: true,
+                source: testFile
+            });
+        });
+
+        it('outcome should be 5k and charged 8 credits', () => {
+            const {
+                charged,
+                dimensions,
+                detected,
+                preserved,
+                size
+            } = outcome;
+
+            expect({
+                charged,
+                dimensions,
+                detected,
+                preserved,
+                size
+            }).to.deep.equal({
+                charged: 8,
+                dimensions: '5184x3456',
+                preserved: true,
+                detected: 'person',
+                size: '4k'
+            });
+
+        });
+
+        it('destination file should be resized', () => {
+            expect(outcome.resized).to.be.true;
+        });
+
+        it('source and destination paths should match', () => {
+            expect(path.dirname(outcome.destination)).to.equal(path.dirname(testFile));
+        });
+
+        it('should save new image in source\'s original directory', async () => {
+            const product = await common.currentFile(outcome.destination);
+            expect(product).to.deep.equal({
+                exists: true,
+                source: outcome.destination
+            });
+        });
+
+        it('source and destinaton file names not should match', () => {
+            expect(sourceFile.dir + `/${sourceFile.name}`).to.equal(destinationFile.dir + `/${destinationFile.name}`);
+        });
+
+        it('source and destinaton extensions should not match', () => {
+            expect(sourceFile.ext).to.equal('.jpg');
+            expect(destinationFile.ext).to.equal('.jpeg');
+        });
+
+        it('saved 5k image should be 5184x3456', async () => {
+            const {
+                width,
+                height
+            } = await common.getDimensions(outcome.destination);
+
+            expect({
+                width,
+                height
+            }).to.deep.equal({
+                width: 5184,
+                height: 3456
+            });
+            await unlink(outcome.destination);
+        });
+
+    });
+
     after(recorder.after);
 
 });
